ed7244d0a66faccc7100fa994e1a6e7b8bdb204b
jaggy1998 <jagjots28@gmail.com>
2023-02-19 19:13:32 +0530

    newCode

--- adminFrontend/src/constants/urls.js
+++ adminFrontend/src/constants/urls.js
@@ -1,3 +1,1 @@
-export const API_URL = "http://localhost:8081/api";
-// export const API_URL =
-//   "http://ec2-50-19-164-137.compute-1.amazonaws.com:8081/api";
+export const API_URL = "http://senstechs.com:8081/api";
